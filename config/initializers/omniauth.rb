f7fd335585cf75c946c3c5d231ce3200f33a2e90
andela-oesho <susan.esho@andela.com>
2016-03-23 00:20:32 +0100

    modified omni auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,8 @@
+require 'openid/fetchers'
+OpenID.fetcher.ca_file = File.join(Rails.root,'config','curl.pem')
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook, "1778754202345754", "172ab9d9a281308dca7b7794b28b13b5"
   provider :google_oauth2, "726222497246-6orbq8fq3rqml2b2q3kvrdpea2htm0ii.apps.googleusercontent.com", "f3i7EDaFfwoObrxFrgWeG5JE",
-  { scope: 'userinfo.email,userinfo.profile,https://www.google.com/m8/feeds', access_type: 'offline', approval_prompt: '', client_options: {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}} }
+  store: OpenID::Store::Filesystem.new(File.join(Rails.root,'tmp'))
   provider :twitter, "M5u41MZ1RCF9CX1stwIvfe1hD", "XJJncoHwffvQsdEfScObLlBDZ0GX93KlL4uiuXirjZIQkiEXOq"
 end 
