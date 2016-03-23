68d1645c3151a0524830faaac2b2a10406b99f2e
andela-oesho <susan.esho@andela.com>
2016-03-23 11:46:28 +0100

    edited scope for omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"],
-           {:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+           { scope: "email", info_fields: "name, email", :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 
 	provider :twitter, ENV["TWITTER_API_KEY"], ENV["TWITTER_SECRET"]
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
