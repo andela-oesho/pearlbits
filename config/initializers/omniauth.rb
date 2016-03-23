13c63e05d801cac3023c2b9d18a11377e5332029
andela-oesho <susan.esho@andela.com>
2016-03-23 11:04:09 +0100

    add environment files

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, "1778754202345754", "172ab9d9a281308dca7b7794b28b13b5"
-	provider :twitter, "M5u41MZ1RCF9CX1stwIvfe1hD", "XJJncoHwffvQsdEfScObLlBDZ0GX93KlL4uiuXirjZIQkiEXOq"
-  provider :google_oauth2, "726222497246-6orbq8fq3rqml2b2q3kvrdpea2htm0ii.apps.googleusercontent.com", "f3i7EDaFfwoObrxFrgWeG5JE" 
+	provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
+	provider :twitter, ENV["TWITTER_API_KEY"], ENV["TWITTER_SECRET"]
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 end 
